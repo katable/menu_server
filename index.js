e189e1ec8a0be4f4efc69f4f7945bfdcfa1304d5
Tony <tnywu@ucdavis.edu>
2018-11-07 06:42:06 -0800

    Implemented proxy server and functional html page. TODO: style page. review and reservation modules not fully functional, to discuss with partners.

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const request = require('request');
+const cors = require('cors');
+const app = express();
+const port = process.env.port || 3005;
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+const infoServer = 'http://localhost:3001';
+const menuServer = 'http://localhost:3000';
+const reviewServer = 'http://localhost:8080';
+const reservationServer = 'http://localhost:3002';
+
+app.get('/restaurant/profile/:restaurant_id', (req, res) => {
+  request(`${infoServer}/restaurant/profile/${req.params.restaurant_id}`, (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      res.send(body);
+    }
+  });
+});
+
+app.get('/restaurants/:restaurant_id/menu', (req, res) => {
+  request(`${menuServer}/restaurants/${req.params.restaurant_id}/menu`, (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      res.send(body);
+    }
+  });
+});
+
+app.get('/restaurant/:restaurant_id/reviews', (req, res) => {
+  request(`${reviewServer}/restaurant/${req.params.restaurant_id}/reviews`, (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      res.send(body);
+    }
+  });
+});
+
+app.get('/reservations/timesBookedToday/:restaurant_id', (req, res) => {
+  request(`${reservationServer}/reservations/timesBookedToday/${req.params.restaurant_id}`, (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      res.send(body);
+    }
+  });
+});
+
+app.get('/reservations/inventory', (req, res) => {
+  request(`${reservationServer}/reservations/inventory`, (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      res.send(body);
+    }
+  })
+});
+
+app.listen(port, () => {
+  console.log(`listening to port ${port}...`);
+});
